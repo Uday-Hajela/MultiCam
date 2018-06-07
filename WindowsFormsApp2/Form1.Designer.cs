ac68d840694629e89c1bcfa54f6152a252fd1c3b
Uday Hajela <uday.hajela@Aveva.com>
2018-06-07 17:54:19 +0530

    First commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Location = new System.Drawing.Point(126, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(309, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(436, 374);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(126, 539);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(309, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(61, 566);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(436, 374);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(796, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(344, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(796, 539);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(344, 21);
+            this.comboBox4.TabIndex = 5;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(740, 39);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(446, 374);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(740, 566);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(446, 374);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1212, 1001);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection VideoCaptureDevices;
+       
+        private VideoCaptureDevice FinalVideoDevice;
+        private VideoCaptureDevice FinalVideoDevice2;
+        private VideoCaptureDevice FinalVideoDevice3;
+        private VideoCaptureDevice FinalVideoDevice4;
+
+        public int j = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            try
+            {
+
+                foreach (FilterInfo VidCapDev in VideoCaptureDevices)
+                {
+                    var i = VidCapDev.MonikerString;
+
+                    j = j + 1;
+                    
+                   
+                    if (j == 1)
+                    {
+                        comboBox1.Items.Add("Camera " + j);
+                        comboBox1.SelectedIndex = 0;
+                        FinalVideoDevice = new VideoCaptureDevice(i);
+                        FinalVideoDevice.NewFrame += new NewFrameEventHandler(FinalVideoDevice_NewFrame);
+                        FinalVideoDevice.Start();
+                    }
+                    if (j == 2)
+                    {
+                        comboBox2.Items.Add("Camera " + j);
+                        comboBox2.SelectedIndex = 0;
+                        FinalVideoDevice2 = new VideoCaptureDevice(i);
+                        FinalVideoDevice2.NewFrame += new NewFrameEventHandler(FinalVideoDevice_NewFrame2);
+                        FinalVideoDevice2.Start();
+                    }
+                    if (j == 3)
+                    {
+                        comboBox3.Items.Add("Camera " + j);
+                        comboBox3.SelectedIndex = 0;
+                        FinalVideoDevice3 = new VideoCaptureDevice(i);
+                        FinalVideoDevice3.NewFrame += new NewFrameEventHandler(FinalVideoDevice_NewFrame3);
+                        FinalVideoDevice3.Start();
+                    }
+                    if (j == 4)
+                    {
+                        comboBox4.Items.Add("Camera " + j);
+                        comboBox4.SelectedIndex = 0;
+                        FinalVideoDevice4 = new VideoCaptureDevice(i);
+                        FinalVideoDevice4.NewFrame += new NewFrameEventHandler(FinalVideoDevice_NewFrame4);
+                        FinalVideoDevice4.Start();
+                    }
+                  
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No camera found. Please connect your camera and click RESET.");
+            }
+
+        }
+        void FinalVideoDevice_NewFrame(object sender, NewFrameEventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = (Bitmap)e.Frame.Clone();
+               
+            }
+            catch { }
+        }
+
+        void FinalVideoDevice_NewFrame2(object sender, NewFrameEventArgs e)
+        {
+            try
+            {
+               
+                pictureBox2.Image = (Bitmap)e.Frame.Clone();
+            }
+            catch { }
+        }
+
+        void FinalVideoDevice_NewFrame3(object sender, NewFrameEventArgs e)
+        {
+            try
+            {
+                pictureBox3.Image = (Bitmap)e.Frame.Clone();
+
+            }
+            catch { }
+        }
+        void FinalVideoDevice_NewFrame4(object sender, NewFrameEventArgs e)
+        {
+            try
+            {
+                pictureBox4.Image = (Bitmap)e.Frame.Clone();
+
+            }
+            catch { }
+        }
+
+
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
